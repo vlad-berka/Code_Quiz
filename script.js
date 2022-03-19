251574974abb78aff1bde345cfc98f6a051fa47c
Vlad Berka Desktop <vlad.m.berka@gmail.com>
2022-03-18 17:28:41 -0700

    Finished code upload

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
+//Initializing question AND answer array
 const list_of_Questions = ["Which of the following equality operators also checks type?", "(=)", "(!==)", "(===)", "(==)",
                         "If we have an array that contains 5 items, which index number corresponds to the last item?", "0", "-L", "5", "4",
-                        "When passing the function named 'thisFunction' as an argument into 'thatFunction', which syntax calling the function is correct?", "thisFunction(thatfunction)", "thatFunction(function thisFunction)", "function(thisFunction, thatFunction)", "thatFunction(thisFunction)",  
-                        "Question 4", "Answer1", "Answer2", "Answer3","Answer4",  
-                        "Question 5", "Answer1", "Answer2", "Answer3","Answer4",  
-                        "Question 6", "Answer1", "Answer2", "Answer3","Answer4",  
-                        "Question 7", "Answer1", "Answer2", "Answer3","Answer4",  
-                        "Question 8", "Answer1", "Answer2", "Answer3","Answer4",  
-                        "Question 9", "Answer1", "Answer2", "Answer3","Answer4",
-                        "Question 10", "Answer1", "Answer2", "Answer3","Answer4"];
+                        "When passing the function named 'thisFunction' as an argument into 'thatFunction', which syntax calling 'thatFunction' is correct?", "thisFunction(thatfunction)", "thatFunction(function thisFunction)", "function(thisFunction, thatFunction)", "thatFunction(thisFunction)",  
+                        "Using JQuery, which of the following methods searches for an element by the ID name 'testID' ?", "document.getElementByID('testID')", "getElementID('testID')", "$('testID')","$('id', 'testID')",  
+                        "What is the form of data pushed into localStorage that is stored as key/value pairs?", "Array", "String", "Map", "Object",  
+                        "During propogation of events through the DOM, what is the name of the phase that travels from the TARGET to the ROOT?", "Bubbling", "Cascading", "Capturing","Triggering",  
+                        "What is the name of the notation for transmitting data between servers and webpages", "JSON", "ASCII", "Hexadecimal","2Bit"];
 
-var answer_key = [2,3,3,2,3,2,2,1,1,1];
+//Initializing the answer key. NOTE: Will populate each question based on length of this key                        
+var answer_key = [2,3,3,2,1,0,0];
+
+
 var question_count = 0;
 var correct_Count = 0;
 var secondsLeft = 0;
 var current_Score = [];
 var high_score_position = 0;
 var scantron = [];
-var secondsLeft = 60;
+var secondsLeft = 910; //note, in deci-seconds, not mili seconds
 var quiz_End = false;
 
 document.querySelector("#begin_button").addEventListener("click", Begin);
 
 function Begin() {
-    Timer();
     document.querySelector(".start_Header").remove();
     document.querySelector(".hidden_Main").setAttribute("class", "not_Hidden_Main");
     document.querySelector(".hidden_List").setAttribute("class", "not_Hidden_List");
@@ -34,14 +34,19 @@ function Begin() {
     document.querySelector("#Answer3").textContent = list_of_Questions[3];
     document.querySelector("#Answer4").textContent = list_of_Questions[4];
 
-    document.querySelector("ul").addEventListener("click", check_Answer);
+    document.querySelector("#Answer1").addEventListener("click", check_Answer);
+    document.querySelector("#Answer2").addEventListener("click", check_Answer);
+    document.querySelector("#Answer3").addEventListener("click", check_Answer);
+    document.querySelector("#Answer4").addEventListener("click", check_Answer);
 
+    Timer();
     // Reset question count and score count in case the user clicks where the buttons are, as they are hidden
     question_count = 0;
 }
 
 function check_Answer() {
     var answer = event.target.getAttribute("id");
+    var id_El = answer;
 
     if (answer == "Answer1")
     {
@@ -64,14 +69,16 @@ function check_Answer() {
     if (answer == answer_key[question_count])
     {
         // Answer IS correct
+        console.log("You're correct! Answered: " +answer+ " and the correct answer was: " +answer_key[question_count]);
         correct_Count++;
+        change_Question();
     } else {
         // Answer NOT correct
-        secondsLeft -= 4;
+        console.log("You're WRONG! Answered: " +answer+ " and the correct answer was: " +answer_key[question_count]);
+        document.querySelector("#"+id_El).setAttribute("style", "background-color: red;");
+        secondsLeft -= 100;
         answer = false;
     }
-
-    change_Question();
 }
 
 function change_Question() {
@@ -84,30 +91,36 @@ function change_Question() {
     } else {    
         var array_pos = question_count*5;
         document.querySelector(".Question").textContent = list_of_Questions[array_pos];
+        document.querySelector("#Answer1").setAttribute("style", "");
         document.querySelector("#Answer1").textContent = list_of_Questions[(array_pos+1)];
+        document.querySelector("#Answer2").setAttribute("style", "");
         document.querySelector("#Answer2").textContent = list_of_Questions[array_pos+2];
+        document.querySelector("#Answer3").setAttribute("style", "");
         document.querySelector("#Answer3").textContent = list_of_Questions[array_pos+3];
+        document.querySelector("#Answer4").setAttribute("style", "");
         document.querySelector("#Answer4").textContent = list_of_Questions[array_pos+4];
     }
 }
 
 function end_Of_Quiz() {
     quiz_End = true;
-    var first_Name = prompt(("You've ended the quiz with " + secondsLeft + " seconds remaining!"), "Enter your name here for high scores!");
+
+    if(secondsLeft=="Timed Out!"){
+        var first_Name = prompt(("You've run out of time!"), "Enter your name here for high scores!");
+    } else {
+        var first_Name = prompt(("You've ended the quiz with " + Math.floor(secondsLeft/10) + " seconds remaining!"), "Enter your name here for high scores!");
+    }
 
     document.body.appendChild(document.createElement("section"));
     document.body.lastChild.setAttribute("id", "final_Screen");
-    document.body.lastChild.textContent = "Name // Score";
+    document.body.lastChild.textContent = "Name - - - Score";
     document.querySelector(".not_Hidden_Score").setAttribute("class", "hidden_Score");
 
-    write_HighScores (first_Name, secondsLeft);
+    write_HighScores (first_Name, Math.floor(secondsLeft/10));
     display_HighScores();
+    document.querySelector(".not_Hidden_Main").setAttribute("class", "final_Screen_Header");
+    document.body.lastChild.appendChild(document.createElement("br"));
 }
-
-// function cleanup_step() {
-//     document.querySelector(".Question").textContent = "CURRENT HIGH SCORES";
-//     document.querySelector(".not_Hidden_List").remove();
-// }
 
 function write_HighScores (first_Name, secondsLeft) {
     var initials = JSON.parse(localStorage.getItem("initials"));
@@ -148,7 +161,7 @@ function write_HighScores (first_Name, secondsLeft) {
 }
 
 function display_HighScores() {
-    document.querySelector(".Question").textContent = "CURRENT HIGH SCORES";
+    document.querySelector(".Question").textContent = "HIGH SCORES!";
     document.querySelector(".not_Hidden_List").remove();
 
     var initials = JSON.parse(localStorage.getItem("initials"));
@@ -157,12 +170,12 @@ function display_HighScores() {
     for (let i = 0; i < initials.length; i++) {
         if (i == high_score_position) {
             document.body.lastChild.appendChild(document.createElement("p"));
-            document.body.lastChild.lastChild.textContent = initials[i] + " // " + init_scores[i];
+            document.body.lastChild.lastChild.textContent = initials[i] + " - - - " + init_scores[i];
             document.body.lastChild.lastChild.setAttribute("id", "your_Score");
         }
         else {
         document.body.lastChild.appendChild(document.createElement("p"));
-        document.body.lastChild.lastChild.textContent = initials[i] + " // " + init_scores[i];
+        document.body.lastChild.lastChild.textContent = initials[i] + " - - - " + init_scores[i];
         }
     }
     document.querySelector(".not_Hidden_Main").appendChild(document.createElement("button"));
@@ -180,17 +193,18 @@ function Timer() {
     var timerInterval = setInterval(function() {
       secondsLeft--;
       
-      if (document.querySelector(".not_Hidden_Score")!= null) {
-         document.querySelector(".not_Hidden_Score").textContent = secondsLeft + " seconds left on the quiz.";
-      }
+    //   if (document.querySelector(".not_Hidden_Score")!= null) {
+         document.querySelector(".not_Hidden_Score").textContent = Math.floor(secondsLeft/10) + " seconds left on the quiz.";
+    //   }
 
       if(secondsLeft <= 0) {
-        // Stops the timer
+        // Stops the timer at 0 or less seconds
+        // For scoring, set text of secondsLeft to Timed Out
+        secondsLeft = "Timed Out!";
         clearInterval(timerInterval);
-        // Ends the quiz if the timer hits 0 or less seconds
         if (quiz_End == false) {
             end_Of_Quiz();
         }
       }
-    }, 1000);
+    }, 100);
   }
